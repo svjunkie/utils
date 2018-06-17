457c2ec557e0225d9bb89b46e7729250acda77cf
John Dryden <john.c.dryden@gmail.com>
2018-06-17 05:09:48 +0000

    update conda portion for multiple environments

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
+
 # configure firewall rules
+# don't forget to configure corresponding EC2 security group rules!
 sudo ufw allow ssh
 sudo ufw enable
 sudo ufw allow 8888
@@ -10,17 +12,34 @@ sudo dd if=/dev/zero of=swapfile bs=1M count=1500
 sudo mkswap swapfile
 sudo swapon swapfile
 
-# MISSING: Install Anaconda
+# MISSING: Install git, screen, (vi?)
+
+# MISSING: Install Anaconda, jupyter
+
+# MISSING: pull config files, anything else needed for setup
 
 # update Anaconda
 conda update -n base conda
 
-# install xgboost for Anaconda
- conda install -c conda-forge xgboost
+# install numpy, scipy, sklearn, xgboost for Anaconda
+conda install -c anaconda numpy
+conda install -c anaconda scipy
+conda install -c anaconda scikit-learn
+conda install -c conda-forge xgboost
+conda install -c conda-forge eli5
 
 # install R and r-essentials for Anaconda
 conda install -c r r-essentials
 
+# if multiple environments are needed, see:
+# http://ipython.readthedocs.io/en/stable/install/kernel_install.html#kernels-for-different-environments
+# conda create --name oldxgb
+# source activate oldxgb
+# conda install -n oldxgb -c conda-forge xgboost=0.6a2
+# conda install pip
+# conda install ipykernel
+# python -m ipykernel install --user --name oldxgb --display-name "Python (oldxgb)"
+
+
 # generate self-signed certificate for public notebook server - interactive
 # openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
-
